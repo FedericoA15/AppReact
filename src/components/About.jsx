a38b19743844c8f475a56ac39ba1d79bb2f9755f
unknown <federicoalmeida15@gmail.com>
2022-12-08 00:25:53 -0300

    V1.2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,19 @@ import styles from "./Styles/About.module.css";
 export default function About(){
     return (
         <div className={styles.prueba}>
-            <h2>Soy Fede</h2>
+            <h3>¿Quien soy?</h3>
+            <p>Buenas, soy Federico Almeida, actual estudiante del bootcamp soy Henry, amante de la programacion y del diseño, buscando poder aprender mas y siempre poder trabajar en grupo con gente nueva</p>
+            <h3>Informacion sobre la aplicacion:</h3>
+            <p>Bueno esta aplicacion la cree para poder practicar temas que vemos en el segundo modulo de soy Henry estas incluyen las siguiente tecnologias</p>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScrip</li>
+                <li>React</li>
+                <li>React-Router-Dom</li>
+                <li>Redux</li>
+                <li>React-Redux</li>
+            </ul>
         </div>
     );
 }
